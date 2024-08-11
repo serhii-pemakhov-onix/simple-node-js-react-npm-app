c3ef87466e09e7f3b867ee0f12e728405b9b5730
Serhii Pemakhov <serhii.pemakhov@onix-systems.com>
2024-08-11 18:19:43 +0300

    feat: edit3

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-          Edit 1
+          Edit 3
         </p>
       </div>
     );
